8c99425308c1397612155ecefb5ab29e6f3e8676
Khira <khirafreeman2029@gmail.com>
2025-06-06 10:04:32 -0500

    Initial Commit

---
+++ BirthDay App/BirthDay_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthDay_AppApp.swift
+//  BirthDay App
+//
+//  Created by Scholar on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthDay_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
